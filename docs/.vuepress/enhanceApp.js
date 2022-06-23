1963a3d6c044f3dfd33cd06d79dbf9e1b0fbcd46
licr1 <licr1@infore.com>
2022-06-23 17:38:18 +0800

    rm: xx

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,5 +1,6 @@
-import Select1 from "./Select.vue";
-import "element-ui/lib/theme-chalk/index.css";
-export default ({ Vue }) => {
-  Vue.component("Select1", Select1);
-};
+// import Select1 from "./Select.vue";
+// import "element-ui/lib/theme-chalk/index.css";
+
+// export default ({ Vue }) => {
+//   Vue.component("Select1", Select1);
+// };
